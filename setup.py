d2ce9b00123b4e92efd774328277187e87d9832f
Robert Smallshire <rob@sixty-north.com>
2020-06-27 21:11:59 +0200

    Reduce the python_requires requirement to 3.8

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
@@ -95,5 +96,5 @@ setup(
         'test': ['coverage', 'hypothesis', 'pytest'],
     },
 
-    python_requires='>=3.8',
+    python_requires='>=3.7',
 )
